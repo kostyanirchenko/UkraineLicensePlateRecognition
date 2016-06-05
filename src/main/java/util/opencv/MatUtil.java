cad241cea592e2f9232bf7cd8cccbdc03ef7046e
kostyanirchenko <nirchenko.kostya@gmail.com>
2016-06-05 18:38:09 +0300

    Обновление проекта

---
+++ src/main/java/util/opencv/MatUtil.java
@@ -0,0 +1,42 @@
+package util.opencv;
+
+import javafx.scene.image.Image;
+import javafx.scene.image.PixelReader;
+import javafx.scene.image.WritablePixelFormat;
+import org.opencv.core.CvType;
+import org.opencv.core.Mat;
+import org.opencv.core.MatOfByte;
+import org.opencv.imgcodecs.Imgcodecs;
+
+import java.io.ByteArrayInputStream;
+import java.nio.ByteBuffer;
+
+/**
+ * Created by Kostya Nirchenko.
+ * @since 20.05.2016
+ */
+public class MatUtil {
+
+    public static Mat mat;
+
+    public static Mat imageToMat(Image image) {
+        int width = (int) image.getWidth();
+        int height = (int) image.getHeight();
+        byte[] buffer = new byte[width * height * 4];
+
+        PixelReader reader = image.getPixelReader();
+        WritablePixelFormat<ByteBuffer> format = WritablePixelFormat.getByteBgraInstance();
+        reader.getPixels(0, 0, width, height, format, buffer, 0, width * 4);
+
+        mat = new Mat(height, width, CvType.CV_8UC4);
+        mat.put(0, 0, buffer);
+        return mat;
+    }
+
+    public static Image matToImage(Mat mat) {
+        MatOfByte buffer = new MatOfByte();
+        Imgcodecs.imencode(".png", mat, buffer);
+        return new Image(new ByteArrayInputStream(buffer.toArray()));
+    }
+
+}
