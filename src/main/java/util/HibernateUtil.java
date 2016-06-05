7158a2de17d7b05d984027454926bae328e0f3be
kostyanirchenko <nirchenko.kostya@gmail.com>
2016-06-06 02:48:23 +0300

    Обновление проекта. Переход на hibernate

--- src/main/java/util/HibernateUtil.java
+++ src/main/java/util/HibernateUtil.java
@@ -1,9 +1,29 @@
 package util;
 
+import org.hibernate.HibernateException;
+import org.hibernate.SessionFactory;
+import org.hibernate.cfg.Configuration;
+import org.hibernate.service.ServiceRegistry;
+import org.hibernate.service.ServiceRegistryBuilder;
+
 /**
  * Created by Kostya Nirchenko.
  * @since 19.05.2016
  */
 public class HibernateUtil {
 
+    private static SessionFactory sessionFactory;
+    private static ServiceRegistry serviceRegistry;
+
+    public static SessionFactory createSessionFactory() throws HibernateException {
+        Configuration configuration = new Configuration();
+        configuration.configure();
+        serviceRegistry = new ServiceRegistryBuilder().applySettings(configuration.getProperties()).buildServiceRegistry();
+        sessionFactory = configuration.buildSessionFactory(serviceRegistry);
+        return sessionFactory;
+    }
+
+    public static SessionFactory getSessionFactory() {
+        return createSessionFactory();
+    }
 }
